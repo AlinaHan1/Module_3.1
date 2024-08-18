004966d33a1a4325f2379ac3570706165a2f30c0
AlinaHan1 <alinakoshenko@gmail.com>
2024-08-19 00:49:54 +0300

    Add files via upload

---
+++ TheFinalBoss_M3.py
@@ -0,0 +1,22 @@
+data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, 6, ({'cube': 7, 'drum': 8}), 'Hello',
+                  ((), [{(2, 'Urban', ('Urban2', 35))}])]
+
+
+def calculate_structure_sum(data_structure):
+    sum_ = []
+    if isinstance(data_structure, dict):  # if data_structure == dict
+        for key, value in data_structure.items():
+            sum_.append(calculate_structure_sum(key))
+            sum_.append(calculate_structure_sum(value))
+    elif isinstance(data_structure, (list, tuple, set)):  # if data_structure == list or tuple or set
+        for item in data_structure:
+            sum_.append(calculate_structure_sum(item))
+    elif isinstance(data_structure, int):  # if data_structure == int or float
+        sum_.append(data_structure)
+    elif isinstance(data_structure, str):  # if data_structure == str
+        sum_.append(len(data_structure))
+    return sum(sum_)
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
