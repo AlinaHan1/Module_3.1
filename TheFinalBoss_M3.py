93b2f6795070adab69083f49bfe1e3601bfc5037
AlinaHan1 <alinakoshenko@gmail.com>
2024-08-19 01:38:06 +0300

    Add files via upload

--- TheFinalBoss_M3.py
+++ TheFinalBoss_M3.py
@@ -3,19 +3,25 @@ data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), 'Hel
 
 
 def calculate_structure_sum(data_structure):
-    sum_ = []
-    if isinstance(data_structure, dict):  # if data_structure == dict
-        for key, value in data_structure.items():
-            sum_.append(calculate_structure_sum(key))
-            sum_.append(calculate_structure_sum(value))
-    elif isinstance(data_structure, (list, tuple, set)):  # if data_structure == list, tuple, set
-        for item in data_structure:
-            sum_.append(calculate_structure_sum(item))
-    elif isinstance(data_structure, int):  # if data_structure == int or float
-        sum_.append(data_structure)
+    summa = 0
+    if isinstance(data_structure, int):  # if data_structure == int, float
+        summa += data_structure
     elif isinstance(data_structure, str):  # if data_structure == str
-        sum_.append(len(data_structure))
-    return sum(sum_)
+        summa += len(data_structure)
+    elif isinstance(data_structure, dict):  # if data_structure == dict
+        for key, value in data_structure.items():
+            summa += calculate_structure_sum(key)
+            summa += calculate_structure_sum(value)
+    elif isinstance(data_structure, list):  # if data_structure == list, tuple, set
+        for item in data_structure:
+            summa += calculate_structure_sum(item)
+    elif isinstance (data_structure, tuple):  # if data_structure == list, tuple, set
+        for item in data_structure:
+            summa += calculate_structure_sum(item)
+    elif isinstance(data_structure, set):  # if data_structure == list, tuple, set
+        for item in data_structure:
+            summa += calculate_structure_sum(item)
+    return summa
 
 
 result = calculate_structure_sum(data_structure)
